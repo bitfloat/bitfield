3c003e97ac4f53229974eac3ae05dd73dfe1cd94
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2023-07-06 20:05:10 +0200

    write function body

--- R/qb_create.R
+++ R/qb_create.R
@@ -1,6 +1,44 @@
+#' Create a new bitfield
+#'
+#' @param width [`integerish(1)`][integer]\cr the table that contains
+#'   \code{test}.
+#' @param length [`integerish(1)`][integer]\cr the column in \code{x} for which
+#'   a range is checked.
+#' @param name [`character(1)`][character]\cr the minimum allowed value.
+#'
+#' @importFrom checkmate assertDataFrame assertSubset assertNumeric
+#' @importFrom rlang new_environment
+#' @importFrom tibble tibble
+#' @export
 
+qb_create <- function(width, length, name = NULL){
 
-qb_create <- function(size = 36){
+  assertIntegerish(x = width, len = 1, lower = 2, any.missing = FALSE, )
+  assertIntegerish(x = length, len = 1, any.missing = FALSE)
+  assertCharacter(x = name, len = 1, null.ok = TRUE)
 
+  # set a name, in case none is provided
+  if(is.null(name)){
+    name <- paste0("qb_", paste0(sample(c(LETTERS, letters), 7, TRUE), collapse = ""), format(Sys.Date(), "%Y%m%d"))
+  }
+
+  # open a new environment
+  .GlobalEnv[["qb_env"]] <- new_environment()
+
+  # put together the intial bitfield
+  out <- new(Class = "bitfield",
+             width = as.integer(width),
+             length = as.integer(length),
+             name = name,
+             desc = tibble(pos = character(), description = character()),
+             bits = list())
+
+  # store the environment name in the options
+  oldOptions <- options()
+  on.exit(options(oldOptions))
+
+  options(qb_env = name)
+
+  return(out)
 
 }
--- R/qb_grow.R
+++ R/qb_grow.R
@@ -1,98 +1,75 @@
 #' Add bits to a bitfield
 #'
-#' @param bit a function that returns
-#' @param desc the description of the bit(s).
-#' @param na which value NAs in the output of .bit should have in the bitfield.
-#' @param pos the position in the bitfield that should be set.
-#' @param bitfield the bitfield in which the bits should be set.
-#' @details
-#' @examples
-#' \dontrun{
-#' library(dplyr)
+#' @param bit [`numeric(.)`][numeric] | [`character(.)`][character] |
+#'   [`logical(1)`][logical]\cr a function that returns a vector that shall be
+#'   stored as bit.
+#' @param name [`character(.)`][character]\cr the internal name of the bit(s).
+#' @param desc [`character(.)`][character]\cr the description of the bit(s).
+#' @param na [`numeric(.)`][numeric] | [`character(.)`][character] |
+#'   [`logical(1)`][logical]\cr which value NAs in the output of .bit should
+#'   have in the bitfield.
+#' @param pos [`integerish(1)`][integer]\cr the position in the bitfield that
+#'   should be set.
+#' @param bitfield [`bitfield(1)`][bitfield]\cr the bitfield in which the bits
+#'   should be set.
 #'
-#' # make an example dataset
-#' input <- tibble(x = sample(seq(23.3, 28.1, 0.1), 10),
-#'                 y = sample(seq(57.5, 59.6, 0.1), 10),
-#'                 year = rep(2021, 10),
-#'                 commodity = rep(c("soybean", "maize"), 5),
-#'                 landuse = sample(c("crop", "grazing", "forest"), size = 10, replace = TRUE),
-#'                 some_other = rnorm(10))
-#'
-#' # make it have some errors
-#' input$x[5] <- 259
-#' input$x[9] <- 0
-#' input$y[10] <- NA_real_
-#' input$y[9] <- 0
-#' input$year[c(2:3)] <- c(NA, "2021r")
-#' input$commodity[c(3, 5)] <- c(NA_character_, "dog")
-#'
-#' # derive valid values for commodities
-#' validComm <- c("soybean", "maize")
-#'
-#' # build a new bitfield
-#' newBitfield <- qb_create()
-#'
-#' # specify the quality checks
-#' newBitfield %>%
-#'   # explicit tests for coordinates ...
-#'   qb_grow(bit = qb_na(x = input, test = "x"),
-#'           desc = c("x-coordinate values do not contain any NAs"),
-#'           pos = 1, bitfield = .) %>%
-#'   qb_grow(bit = qb_range(x = input, test = "x", min = -180, max = 180),
-#'           desc = c("x-coordinate values are numeric and within the valid WGS84 range"),
-#'           pos = 2, bitfield = .) %>%
-#'   # ... or override NA test
-#'   qb_grow(bit = qb_range(x = input, test = "y", min = -90, max = 90),
-#'           desc = c("y-coordinate values are numeric and within the valid WGS84 range, NAs are FALSE"),
-#'           pos = 3, na = FALSE, bitfield = .) %>%
-#'   # it is also possible to use other functions that give flags, such as from CoordinateCleaner ...
-#'   qb_grow(bit = cc_equ(x = input, lon = "x", lat = "y", value = "flagged"),
-#'           desc = c("x and y coordinates are not identical"),
-#'           pos = 4, bitfield = .) %>%
-#'   qb_grow(bit = qb_na(x = input, test = "year"),
-#'           desc = c("year values do not contain any NAs"),
-#'           pos = 5, bitfield = .) %>%
-#'   # ... or stringr
-#'   qb_grow(bit = str_detect(input$year, "r"),
-#'           desc = c("year values do have a flag"),
-#'           pos = 6, bitfield = .)  %>%
-#'   # test for matches with an external vector
-#'   qb_grow(bit = qb_match(x = input, test = "commodity", against = validComm),
-#'           desc = c("commodity values are part of 'soybean' or 'maize'"),
-#'           na = FALSE, pos = 7, bitfield = .) %>%
-#'   # define cases
-#'   qb_grow(bit = qb_case(x = input, some_other > 0.5, some_other > 0, some_other < 0),
-#'           desc = c("some_other values are distinguished into large, medium and small"),
-#'           pos = 8:9, bitfield = .)
-#'
-#' @importFrom checkmate assertChoice assertIntegerish
+#' @importFrom checkmate assertDataFrame assertChoice assertIntegerish
+#'   assertTRUE
+#' @importFrom rlang env_bind
 #' @export
 
+qb_grow <- function(bit, name, desc = NULL, na = NULL, pos, bitfield){
 
-qb_grow <- function(bit, desc, na = FALSE, pos, bitfield){
-
-  assertChoice(x = na, choices = unique(bit))
+  # assertions
+  assertDataFrame(x = bit, ncols = 1)
+  assertCharacter(x = name, len = 1, any.missing = FALSE)
+  assertCharacter(x = desc, null.ok = TRUE)
   assertIntegerish(x = pos, lower = 1, min.len = 1, unique = TRUE)
 
+  # test whether the number of flags corresponds to the number of positions provided
+  theValues <- bit %>%
+    pull(1)
 
-  # https://stackoverflow.com/questions/62333478/from-integer-to-bits-and-back-again
-  # 'intToBase2' <- function(x){
-  #   x %>%
-  #     intToBits() %>%
-  #     rev %>%
-  #     as.character() %>%
-  #     {sapply(strsplit(., "", fixed = TRUE), '[', 2)} %>%
-  #     paste0(.,collapse = '')
-  # }
-  #
-  # base2ToInt <- function(string){
-  #   sapply(strsplit(string, ""), function(x) sum(rev(+(x == "1")) * 2^(seq(x)-1)))
-  # }
+  # replace NA values in theValues
+  if(any(is.na(theValues))){
+    assertClass(x = class(na), classes = class(theValues))
+    theValues[is.na(theValues)] <- na
+  }
 
+  if(is.logical(theValues)){
+    # this is needed because not always both TRUE and FALSE are in the vector
+    nFlags <- 2
+    outValues <- c(TRUE, FALSE)
+  } else {
+    nFlags <- theValues %>%
+      unique() %>%
+      sqrt() %>%
+      ceiling()
+    outValues <- theValues
+  }
+  nBits <- ceiling(log2(nFlags))
 
-  # number of flags
-  flags <- ceiling(sqrt(...)) # of unique values in the bit
+  assertTRUE(x = nBits == length(pos))
 
-  return()
+  # handle descriptions
+  theDesc <- bitfield@desc
+  if(is.null(desc)){
+    message(paste0("please provide a description for ", name))
+  } else {
+    outDesc <- tibble(pos = as.character(pos), description = desc) %>%
+      bind_rows(theDesc, .)
+  }
+
+  # assign tentative bit values into the current environment
+  env_bind(.env = qb_env, !!name := theValues)
+
+  # and store everything in the bitfield
+  temp <- list(values = outValues,
+               position = pos)
+
+  bitfield@bits[[name]] <- temp
+  bitfield@desc <- outDesc
+
+  return(bitfield)
 
 }
