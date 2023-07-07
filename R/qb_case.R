60ac595c66de1c7698a938ad150896a621c86eed
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2023-07-07 19:08:34 +0200

    write function

--- R/qb_case.R
+++ R/qb_case.R
@@ -8,7 +8,11 @@
 #'   that the cases are mutually exclusive, or whether it would allow that cases
 #'   defined later in the sequence overwrite cases earlier in the sequence.
 #'
-#' @importFrom checkmate assertDataFrame assertLogical
+#' @importFrom checkmate assertDataFrame assertLogical assertTRUE
+#' @importFrom rlang enquos eval_tidy as_label
+#' @importFrom purrr map reduce
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr rename bind_cols filter if_else
 #' @export
 
 qb_case <- function(x, ..., exclusive = TRUE){
@@ -16,6 +20,34 @@ qb_case <- function(x, ..., exclusive = TRUE){
   assertDataFrame(x = x)
   assertLogical(x = exclusive, len = 1)
 
+  cases <- enquos(...)
 
+  temp <- map(cases, function(ix){
+    eval_tidy(expr = ix, data = x) %>%
+      as_tibble() %>%
+      rename(!!as_label(ix) := value)
+  }) %>%
+    bind_cols()
+
+  if(exclusive){
+
+    test <- temp %>%
+      filter(rowSums(temp) != 1)
+    assertTRUE(x = dim(test)[1] == 0, .var.name = "overlapping columns == 0")
+
+  }
+
+  status <- map(seq_along(temp), function(ix){
+    temp[[ix]][temp[[ix]]] <- ix
+    temp[ix] %>%
+      as_tibble()
+  }) %>%
+    bind_cols()
+
+  out <- reduce(status, function(first, second){
+    if_else(second != 0, second, first)
+  })
+
+  return(out)
 
 }
