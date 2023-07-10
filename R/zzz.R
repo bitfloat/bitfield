8cfd476adbc34824a411b866b7dd3ae3baeef50f
EhrmannS <steffen.ehrmann@posteo.de>
2023-07-10 13:48:52 +0200

    define global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 globalVariables(c("avail", "coords", "value", "bits", "xAvail", "yAvail",
                   "xRange", "yRange", "flag", ".", "x", "y", "pos", "qb_env",
-                  "QB_insepct", "distinct", "temp"))
+                  "QB_insepct", "distinct", "temp", "name", "flags", "QB",
+                  "bit"))
