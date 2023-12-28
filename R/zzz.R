a6f18750de977c45e978d18f3f4af5cffecf0d26
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2023-12-28 20:07:32 +0100

    .

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 globalVariables(c("avail", "coords", "value", "bits", "xAvail", "yAvail",
-                  "xRange", "yRange", "flag", ".", "x", "y", "pos", "qb_env",
+                  "xRange", "yRange", "flag", ".", "x", "y", "pos", "bf_env",
                   "QB_insepct", "distinct", "temp", "name", "flags", "QB",
                   "bit"))
