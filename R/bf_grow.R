d86345ff01e647e6e9d0dd7ee00775b8eaadf435
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-02-20 20:52:48 +0100

    revise documentation

--- R/bf_grow.R
+++ R/bf_grow.R
@@ -2,18 +2,35 @@
 #'
 #' @param flags [`function(.)`][function]\cr a function that returns a vector
 #'   that shall be stored as bit.
-#' @param name [`character(.)`][character]\cr the internal name of the bit
+#' @param name [`character(1)`][character]\cr the internal name of the bit
 #'   flag(s).
-#' @param desc [`character(.)`][character]\cr the description of the bit
+#' @param desc [`character(1)`][character]\cr the description of the bit
 #'   flag(s).
-#' @param na_val [`numeric(.)`][numeric] | [`character(.)`][character] |
+#' @param na_val [`numeric(1)`][numeric] | [`character(1)`][character] |
 #'   [`logical(1)`][logical]\cr which value NAs in the output of \code{flags}
 #'   should have in the registry.
-#' @param pos [`integerish(1)`][integer]\cr the position in the bitfield that
+#' @param pos [`integerish(.)`][integer]\cr the position(s) in the bitfield that
 #'   should be set.
 #' @param registry [`registry(1)`][registry]\cr the registry in which the bit
 #'   flag(s) should be stored.
-#'
+#' @details The following set of flags can be built with \code{bitfield}
+#' operators: \itemize{
+#' \item flags where a column is tested against some condition, such as
+#'   \code{\link{bf_na}}, \code{\link{bf_nan}}, \code{\link{bf_null}} and
+#'   \code{\link{bf_inf}} (output length 1).
+#' \item flags where an attribute is identified from a small set of possible
+#'   choices, such as \code{\link{bf_decimals}}, \code{\link{bf_length}} or
+#'   \code{\link{bf_type}} (output length 1).
+#' \item flags where a column is compared with another column (of the same
+#'   length) or a set of values (of another length), such as
+#'   \code{\link{bf_identical}}, \code{\link{bf_match}} and
+#'   \code{\link{bf_range}} (output length 1).
+#' \item flags where a test that results in a small set of categorical values
+#'   that are combined into a compound flag, such as \code{\link{bf_case}}
+#'   (output length > 1)
+#' \item flags where relation between objects is recorded, including the output
+#'   of other bitfield operators.
+#' }
 #' @importFrom checkmate assertCharacter assertClass assertIntegerish assertTRUE
 #' @importFrom rlang env_bind
 #' @importFrom dplyr arrange distinct bind_rows
