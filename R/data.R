9fde13a5c545aef35d79f41aea512af7bfb2c19c
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-02-20 20:51:13 +0100

    incl. example data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Example table
+#'
+#' A 10 × 5 tibble with a range of example data to showcase functionality of
+#' this package
+#' @format object of class \code{tibble} has two columns that indicate
+#'   coordinates, one column that indicates a crop that is grown there, one
+#'   column that indicates the yield of that crop there and one column that
+#'   indicates the year of harvest. All columns contain some sort of deviation
+#'   that may occur in data.
+"example_data"
