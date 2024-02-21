822011be565ee801cf798692ee790f68f1ec2b9c
EhrmannS <steffen.ehrmann@posteo.de>
2024-02-22 00:34:40 +0100

    include new slots and adapte handling of width and length of bitfield

--- R/bf_create.R
+++ R/bf_create.R
@@ -1,37 +1,48 @@
-#' Create a new bitfield
+#' Create a new registry
 #'
-#' @param width [`integerish(1)`][integer]\cr the table that contains
-#'   \code{test}.
-#' @param length [`integerish(1)`][integer]\cr the column in \code{x} for which
-#'   a range is checked.
-#' @param name [`character(1)`][character]\cr the minimum allowed value.
+#' @param length [`integerish(1)`][integer]\cr how many observation there shall
+#'   be in the bitfield encoded by this registry.
+#' @param name [`character(1)`][character]\cr the name of the bitfield.
+#' @param description [`character(1)`][character]\cr the description of the
+#'   bitfield.
 #'
 #' @importFrom checkmate assertDataFrame assertSubset assertNumeric
+#' @importFrom utils packageVersion
+#' @importFrom stringr str_replace_all
 #' @importFrom rlang new_environment
 #' @importFrom tibble tibble
 #' @importFrom methods new
 #' @export
 
-bf_create <- function(width, length, name = NULL){
+bf_create <- function(length = NULL, name = NULL, description = NULL){
 
-  assertIntegerish(x = width, len = 1, lower = 2, any.missing = FALSE, )
-  assertIntegerish(x = length, len = 1, any.missing = FALSE)
+  assertIntegerish(x = length, len = 1, null.ok = TRUE)
   assertCharacter(x = name, len = 1, null.ok = TRUE)
+  assertCharacter(x = description, len = 1, null.ok = TRUE)
+
+  if(is.null(length)) length <- 0
+  if(is.null(description)) description <- NA_character_
 
   # set a name, in case none is provided
   if(is.null(name)){
-    name <- paste0("bf_", paste0(sample(c(LETTERS, letters), 7, TRUE), collapse = ""), format(Sys.Date(), "%Y%m%d"))
+    name <- paste0("bf_", paste0(sample(c(LETTERS, letters), 12, TRUE), collapse = ""))
   }
+
+  bf_version <- str_replace_all(string = as.character(packageVersion("bitfield")), pattern = "[.]", replacement = "")
+  r_version <- str_replace_all(string = paste0(version$major, version$minor), pattern = "[.]", replacement = "")
+  version <- paste0(paste0(c(bf_version, r_version, format(Sys.Date(), "%Y%m%d")), collapse = "."))
 
   # open a new environment
   .GlobalEnv[["bf_env"]] <- new_environment()
 
   # put together the intial bitfield
   out <- new(Class = "registry",
-             width = as.integer(width),
+             width = 0L,
              length = as.integer(length),
              name = name,
-             # desc = tibble(pos = integer(), description = character()),
+             version = version,
+             md5 = NA_character_,
+             description = description,
              flags = list())
 
   # store the environment name in the options
