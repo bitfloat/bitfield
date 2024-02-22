2fa6493684189b78589849b2706c34ef0bb79f78
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-02-22 18:29:34 +0100

    wip

--- R/bf_origin.R
+++ R/bf_origin.R
@@ -1,6 +1,13 @@
+#' Build a bit flag from an operation that has generated the focal entity
+#'
+#' @param x [`data.frame(1)`][data.frame]\cr the table that contains...
+#' @importFrom checkmate assertDataFrame
+#' @export
 
+bf_origin <- function(x){
 
-bf_origin <- function(){
+  assertDataFrame(x = x)
+
 
 
 
--- R/bf_residuals.R
+++ R/bf_residuals.R
@@ -1,13 +1,16 @@
 #' Build a bit flag from the residuals of a model
 #'
-#' @param x [`data.frame(1)`][data.frame]\cr the table that contains columns
+#' @param x [`data.frame(1)`][data.frame]\cr the table that contains a column
 #'   with the residuals.
 #' @param resid [`character(1)`][character]\cr the column in \code{x} that
 #'   contains the residuals.
-#' @importFrom checkmate assertDataFrame assertCharacter
+#' @importFrom checkmate assertDataFrame assertSubset
+#' @export
 
 bf_residuals <- function(x, resid){
 
+  assertDataFrame(x = x)
+  assertSubset(x = resid, choices = names(x))
 
 
 
