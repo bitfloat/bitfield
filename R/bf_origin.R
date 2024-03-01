40823f7cad342160001fac83c286d7afd60af84f
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-03-01 18:28:51 +0100

    wip

--- R/bf_origin.R
+++ R/bf_origin.R
@@ -1,14 +1,19 @@
 #' Build a bit flag from an operation that has generated the focal entity
 #'
 #' @param x [`data.frame(1)`][data.frame]\cr the table that contains...
-#' @importFrom checkmate assertDataFrame
+#' @param test [`character(1)`][character]\cr something
+#' @importFrom checkmate assertDataFrame assertCharacter
 #' @export
 
-bf_origin <- function(x){
+bf_origin <- function(x, test){
 
-  assertDataFrame(x = x)
-
-
-
-
+  # assertDataFrame(x = x)
+  # assertCharacter(x = test, len = 1, any.missing = FALSE)
+  #
+  # example_data[test] <- rnorm(10)
+  #
+  # savehistory()
+  # hist <- readLines(".Rhistory")
+  #
+  # return(hist)
 }
