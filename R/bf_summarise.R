d5572961b89b6b26ceb460aca5365c6ec52d3797
EhrmannS <steffen.ehrmann@posteo.de>
2024-03-04 23:53:35 +0100

    .

--- R/bf_summarise.R
+++ R/bf_summarise.R
@@ -1,6 +1,16 @@
+#' Build a bit flag from the summary of ...
+#'
+#' @param x [`data.frame(1)`][data.frame]\cr the table that contains a column
+#'   with the residuals.
+#' @param sum [`character(1)`][character]\cr the column in \code{x} that ...
+#' @importFrom checkmate assertDataFrame assertSubset
+#' @export
 
+bf_summarise <- function(x, sum){
 
-bf_summarise <- function(){
+  assertDataFrame(x = x)
+  assertSubset(x = sum, choices = names(x))
+
 
 
 }
