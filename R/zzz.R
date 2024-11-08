787af8d624e477b6f44bd9e69d9b37a666c3deb6
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-11-08 18:04:20 +0100

    more global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 globalVariables(c("avail", "coords", "value", "bits", "xAvail", "yAvail",
                   "xRange", "yRange", "flag", ".", "x", "y", "pos", "bf_env",
                   "distinct", "temp", "name", "flags", "bf_int",
-                  "bit", "desc", "bin"))
+                  "bit", "desc", "bin", "grp"))
