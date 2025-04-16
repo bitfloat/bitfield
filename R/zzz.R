5b488528ddeb7e1aae6caad6f020400be62577a1
EhrmannS <steffen.ehrmann@posteo.de>
2025-04-16 13:45:44 +0200

    update global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 globalVariables(c("avail", "coords", "value", "bits", "xAvail", "yAvail",
                   "xRange", "yRange", "flag", ".", "x", "y", "pos", "bf_env",
                   "distinct", "temp", "name", "flags", "bf_int",
-                  "bit", "desc", "bin", "grp"))
+                  "bit", "desc", "bin", "grp", "bf_internal", "bf_opr", "fields",
+                  "decimals", "vals"))
