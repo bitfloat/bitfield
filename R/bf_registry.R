9abfae4977d63bf307af3be5e39dde7972c3aacd
EhrmannS <steffen.ehrmann@posteo.de>
2025-07-13 14:21:09 +0200

    include project documentation

--- R/bf_registry.R
+++ R/bf_registry.R
@@ -3,13 +3,32 @@
 #' @param name [`character(1)`][character]\cr the name of the bitfield.
 #' @param description [`character(1)`][character]\cr the description of the
 #'   bitfield.
+#' @param author [`person(.)`][person]\cr the author(s) involved in the creation
+#'   of this registry.
+#' @param project [`list(1)`][list]\cr object created with the function
+#'   \code{\link{project}} that documents the project metadata.
+#' @param license [`character(1)`][character]\cr license or rights statement.
 #' @return an empty registry that captures some metadata of the bitfield, but
 #'   doesn't contain any flags yet.
 #' @examples
+#' me <- person(given = "Jane", family = "Smith",
+#'              email = "jane@example.com", role = c("cre", "aut"))
+#'
+#' myProj <- project(title = "example project",
+#'                   people = c(person("Jane", "Smith", email = "jane@example.com",
+#'                                     role = "aut"),
+#'                              person("Robert", "Jones", role = c("aut", "cre")),
+#'                   publisher = "example publisher",
+#'                   type = "Dataset",
+#'                   identifier = "10.5281/zenodo.1234567",
+#'                   description = "A comprehensive explanation",
+#'                   subject = c("keyword", "subject"),
+#'                   license = "CC-BY-4.0")
+#'
 #' reg <- bf_registry(name = "currentWorkflow",
-#'                    description = "this is to document my current workflow so
-#'                                  that I can share it with my colleagues
-#'                                  alongside a publication.")
+#'                    description = "the registry to my modelling pipeline",
+#'                    author = me,
+#'                    project = myProj)
 #' @importFrom checkmate assertIntegerish assertCharacter
 #' @importFrom utils packageVersion
 #' @importFrom stringr str_replace_all
@@ -18,10 +37,12 @@
 #' @importFrom methods new
 #' @export
 
-bf_registry <- function(name = NULL, description = NULL){
+bf_registry <- function(name, description, author = NULL, project = NULL,
+                        license = "MIT"){
 
   assertCharacter(x = name, len = 1, null.ok = TRUE)
   assertCharacter(x = description, len = 1, null.ok = TRUE)
+  assertClass(author, classes = "person")
 
   if(is.null(description)) description <- NA_character_
 
@@ -40,7 +61,10 @@ bf_registry <- function(name = NULL, description = NULL){
              version = version,
              md5 = NA_character_,
              description = description,
-             flags = list())
+             flags = list(),
+             metadata = list(author = author,
+                             project = project,
+                             license = license))
 
   return(out)
 
