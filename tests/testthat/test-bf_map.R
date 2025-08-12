dcb8efa12fae45f18db31caeccbbeb49f117f34c
EhrmannS <steffen.ehrmann@posteo.de>
2025-08-12 23:28:38 +0200

    wip

--- tests/testthat/test-bf_map.R
+++ tests/testthat/test-bf_map.R
@@ -1,3 +1,29 @@
+test_that("test encoding type 'bool'", {
+
+
+})
+
+test_that("test encoding type 'enum'", {
+
+
+})
+
+test_that("test encoding type 'int'", {
+
+
+})
+
+test_that("test encoding type 'float'", {
+
+
+})
+
+test_that("test protocol 'case'", {
+
+
+})
+
+
 test_that("bf_map writes correct registry", {
 
   reg <- bf_registry(name = "testBF", description = "test bitfield")
@@ -8,28 +34,27 @@ test_that("bf_map writes correct registry", {
   # check registry
   expect_s4_class(reg, "registry")
   expect_equal(reg@width, 1L)
-  expect_equal(reg@length, 10L)
+  expect_equal(reg@length, 9L)
   expect_equal(reg@name, "testBF")
 
   # check flags
   expect_true("na_y" %in% names(reg@flags))
-  expect_equal(reg@flags$na_y$pos, 1)
-  expect_equal(reg@flags$na_y$encoding$sign, 0)
-  expect_equal(reg@flags$na_y$encoding$exponent, 0)
-  expect_equal(reg@flags$na_y$encoding$mantissa, 1)
-  expect_equal(reg@flags$na_y$encoding$bias, 0)
+  expect_equal(reg@flags$na_y$wasGeneratedBy$assignPosition, c(1, 1))
+  expect_equal(reg@flags$na_y$wasGeneratedBy$encodeAsBinary$sign, 0)
+  expect_equal(reg@flags$na_y$wasGeneratedBy$encodeAsBinary$exponent, 0)
+  expect_equal(reg@flags$na_y$wasGeneratedBy$encodeAsBinary$significand, 1)
+  expect_equal(reg@flags$na_y$wasGeneratedBy$encodeAsBinary$bias, 0)
 
   # test other metadata
-  expect_true(all(c("wasDerivedFrom", "wasGeneratedBy", "wasAssociatedWith") %in% names(reg@flags$na_y$provenance)))
-  expect_equal(unlist(reg@flags$na_y$provenance, use.names = F)[1:4], c("bf_tbl", "useTest: na_1.0.0", "withArguments: x=y", "encodeAsBinary: 0.0.1/0"))
-  expect_equal(reg@flags$na_y$description, c("'y' contains NA-values."))
+  expect_true(all(c("comment", "wasDerivedFrom", "wasGeneratedBy", "wasAssociatedWith") %in% names(reg@flags$na_y)))
+  expect_equal(reg@flags$na_y$comment, c("'y' contains NA-values."))
 
   # test updating an existing registry
   reg <- bf_map(protocol = "na", data = bf_tbl, x = year, registry = reg)
   expect_equal(reg@width, 2L)
-  expect_equal(reg@length, 10L)
+  expect_equal(reg@length, 9L)
   expect_true("na_year" %in% names(reg@flags))
-  expect_equal(reg@flags$na_year$pos, 2)
+  expect_equal(reg@flags$na_year$wasGeneratedBy$assignPosition, c(2, 2))
   expect_true(length(reg@flags) == 2L)
 
 })
@@ -41,28 +66,22 @@ test_that("bf_map write the correct object into bf_env", {
   # run protocol
   reg <- bf_map(protocol = "na", data = bf_tbl, registry = reg, x = x)
 
-  # test that the intermediate output is correct
-  expect_equal(bf_env$na_x, c(F, F, F, F, F, F, F, F, F, F))
-
 })
 
 test_that("bf_map handles rast objects correctly", {
 
   reg <- bf_registry(name = "testBF", description = "test bitfield")
 
-  bf_rst <- rast(nrows = 3, ncols = 3, vals = as.integer(c(1, 2, 3, NA, 5, 6, 7, 8, 9)))
+  bf_rst <- terra::rast(nrows = 3, ncols = 3, vals = as.integer(c(1, 2, 3, NA, 5, 6, 7, 8, 9)))
 
   # run protocol
-  reg <- bf_map(protocol = "na", data = .rast(bf_rst), registry = reg, x = lyr.1)
+  reg <- bf_map(protocol = "na", data = bf_rst, registry = reg, x = lyr.1)
 
   # check registry
   expect_s4_class(reg, "registry")
   expect_equal(reg@width, 1L)
   expect_equal(reg@length, 9L)
   expect_equal(reg@name, "testBF")
-
-  # test that the intermediate output is correct
-  expect_equal(bf_env$na_lyr.1, c(F, F, F, T, F, F, F, F, F))
 
 })
 
---
+++ tests/testthat/test-bf_pcl.R
@@ -0,0 +1,70 @@
+test_that("test bc_pcl$na", {
+
+
+})
+
+test_that("test bc_pcl$nan", {
+
+
+})
+
+test_that("test bc_pcl$inf", {
+
+
+})
+
+test_that("test bc_pcl$identical", {
+
+
+})
+
+test_that("test bc_pcl$range", {
+
+
+})
+
+test_that("test bc_pcl$matches", {
+
+
+})
+
+test_that("test bc_pcl$grepl", {
+
+
+})
+
+test_that("test bc_pcl$category", {
+
+
+})
+
+test_that("test bc_pcl$case", {
+
+
+})
+
+test_that("test bc_pcl$nChar", {
+
+
+})
+
+test_that("test bc_pcl$nInt", {
+
+
+})
+
+test_that("test bc_pcl$Dnec", {
+
+
+})
+
+test_that("test bc_pcl$integer", {
+
+
+})
+
+test_that("test bc_pcl$numeric", {
+
+
+})
+
